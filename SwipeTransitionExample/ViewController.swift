17e2b796654359fbf81ef8f15eea01743e06755e
kukushi <17washabi@gmail.com>
2015-02-18 00:13:47 +0800

    Initial Commit

---
+++ SwipeTransitionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeTransitionExample
+//
+//  Created by kukushi on 2/18/15.
+//  Copyright (c) 2015 kukushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
